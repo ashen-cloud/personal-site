e34c3a223dfdadfebc3bab61bf99bf95b11fb9dd
ratking1337 <serendipity1337@protonmail.com>
2020-06-03 20:00:53 +0300

    init

---
+++ main.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { getPosts, formatName, getData } = require('./post_loader');
+
+const app = express();
+
+const postFolder = "./public/posts";
+
+const posts = getPosts(postFolder);
+
+// app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('main.ejs', {
+    title: "My site",
+    posts,
+    formatName
+  });
+});
+
+for (const post of posts) {
+  app.get('/post/' + post, (req, res) => {
+    res.render('post.ejs', {
+      title: formatName(post),
+      data: getData(`${postFolder}/${post}`)
+    });
+  })
+}
+
+app.listen(8080);
+console.log('Working on port 8080');
---
+++ post_loader.js
@@ -0,0 +1,19 @@
+const fs = require('fs-extra')
+
+function formatName(name) {
+  name = name.slice(0, name.indexOf(".")).replace(
+    /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, ' '
+  );
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+function getPosts(path) {
+  return fs.readdirSync(path);
+}
+
+function getData(path) {
+  return fs.readFileSync(path);
+}
+
+module.exports = { getPosts, formatName, getData };
+
